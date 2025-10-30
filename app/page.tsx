ceb9a4d51518efcb4c2804c7ed627d6bad7d60f8
turbokuzmich <kurteev.d@yandex.ru>
2025-10-30 09:40:09 +0300

    header and basic hero block wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,88 @@
-import Image from "next/image";
+import Link from "next/link";
+import { PhoneIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <>
+      {/* Header */}
+      <header className="sticky top-0 z-50 bg-white">
+        <nav className="px-2 md:px-5 2xl:px-0">
+          <div className="flex items-center justify-between gap-20 max-w-7xl mx-auto py-5">
+            <div className="flex items-center gap-4">
+              <Link href="/">
+                <img src="/vercel.svg" alt="Профипол" className="w-16" />
+              </Link>
+              <span className="text-xs font-normal">
+                Профессиональная
+                <br />
+                стяжка пола
+              </span>
+            </div>
+            <div className="flex items-center gap-10">
+              <div className="flex items-center gap-8 text-base">
+                <a href="/" className="hover:underline">
+                  Цены
+                </a>
+                <a href="/" className="hover:underline">
+                  Работы
+                </a>
+                <a href="/" className="hover:underline">
+                  Контакты
+                </a>
+              </div>
+              <a
+                href="tel:+79991234567"
+                className="flex items-center gap-2 text-sky-600 hover:text-sky-800 transition-colors"
+              >
+                <PhoneIcon className="w-5 h-5" />
+                <span className="font-medium">+7 (999) 123-45-67</span>
+              </a>
+              <button
+                type="button"
+                className="bg-sky-600 hover:bg-sky-800 text-white px-6 py-4 rounded-full cursor-pointer"
+              >
+                Оставить заявку
+              </button>
+            </div>
+          </div>
+        </nav>
+      </header>
+
+      {/* Hero */}
+      <section className="px-2 md:px-5 2xl:px-0">
+        <div className="max-w-7xl mx-auto pt-40">
+          <h1 className="text-7xl font-bold tracking-tight text-center mb-5">
+            Профессиональная стяжка пола
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+          <div className="flex justify-center mb-10">
+            <p className="text-2xl font-normal text-center max-w-2xl leading-10">
+              С пожизненной гарантией в Москве и Московской области от 280 ₽ за
+              м²
+            </p>
+          </div>
+          <div className="flex justify-center gap-4">
+            <button
+              type="button"
+              className="bg-sky-600 hover:bg-sky-800 text-white px-6 py-4 rounded-full cursor-pointer"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              Оставить заявку
+            </button>
+            <button
+              type="button"
+              className="bg-white text-sky-600 hover:text-sky-800 border border-sky-600 px-6 py-4 rounded-full cursor-pointer"
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              Узнать цены
+            </button>
+          </div>
+
+          {/* Форма для заявки на стяжку пола */}
+          <div></div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </section>
+
+      <section className="px-2 md:px-5 2xl:px-0 bg-neutral-100">
+        <div className="max-w-7xl mx-auto py-40">Тут дальше идет контент</div>
+      </section>
+    </>
   );
 }
